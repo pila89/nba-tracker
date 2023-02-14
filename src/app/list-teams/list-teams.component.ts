868549ad181709c9a0aafe2c591a0ed8e0e0818a
pila89 <salim.amara@outlook.com>
2023-02-14 22:57:05 +0100

    fix average score

--- src/app/list-teams/list-teams.component.ts
+++ src/app/list-teams/list-teams.component.ts
@@ -47,7 +47,7 @@ export class ListTeamsComponent implements OnInit {
     }
     this.nbaService.getGamesByIdTeam(this.teamForm.value.teamId).subscribe(
       (response) => {
-        response.idTeam=this.teamForm.value.teamId;
+        response.idTeam = this.teamForm.value.teamId;
         this.gameResponses.push(response);
         // this.teamForm.reset();
         this.submitted = false;
@@ -62,23 +62,35 @@ export class ListTeamsComponent implements OnInit {
     this.gameResponses.splice(i, 1);
   }
 
-  getAvregeScore(i: number): number {
+  getAvregeScore(i: number, idTeam: number | undefined): number {
     const score = this.gameResponses[i].data
-      .map((game) => game.home_team_score)
+      .map((game) => {
+        if (game.home_team.id == idTeam) {
+          return game.home_team_score;
+        } else {
+          return game.visitor_team_score;
+        }
+      })
       .reduce((a, b) => {
         return a + b;
       }, 0);
     const total = this.gameResponses[i].data.length;
-    return Math.round(score / total);
+    return Math.trunc(score / total);
   }
 
-  getCancededScore(i: number): number {
+  getCancededScore(i: number, idTeam: number | undefined): number {
     const score = this.gameResponses[i].data
-      .map((game) => game.visitor_team_score)
+      .map((game) => {
+        if (game.home_team.id == idTeam) {
+          return game.visitor_team_score;
+        } else {
+          return game.home_team_score;
+        }
+      })
       .reduce((a, b) => {
         return a + b;
       }, 0);
     const total = this.gameResponses[i].data.length;
-    return Math.round(score / total);
+    return Math.trunc(score / total);
   }
 }
